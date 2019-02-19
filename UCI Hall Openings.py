7e14007aa3f89d9f65b89638f9caa4ff4e5534b8
omar1vega <omar1vega@gmail.com>
2019-02-18 19:25:46 -0800

    applied pep8 formatting

--- UCI Hall Openings.py
+++ UCI Hall Openings.py
@@ -5,15 +5,18 @@ import requests
 """
 
 
-def getClasses():
+def get_classes():
     # Downloads Entire Web Schedule of Classes Database, by course code range (1000-99999), saves to a file
     with open('WebSocFile.txt', 'a') as webSocFile:
         for i in range(1, 100):
-            rangeStart = i * 1000
-            rangeStop = rangeStart + 999
-            ranges = str(rangeStart) + '-' + str(rangeStop)
+            range_start = i * 1000
+            range_stop = range_start + 999
+            ranges = str(range_start) + '-' + str(range_stop)
 
-        url = 'https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=' + ranges + '&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text+Results'
+            url = 'https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY' \
+                  '&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=' + ranges + \
+                  '&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY' \
+                  '&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text+Results '
 
             classes = requests.get(url).text
             webSocFile.write(classes)
@@ -21,43 +24,43 @@ def getClasses():
 
 
 def main():
-    searchTerm = input("Enter a Hall/Classroom to search for, e.g. 'HIB 100' , 'SSLH 100' \n").upper()
-    timesOpenList = []
+    search_term = input("Enter a Hall/Classroom to search for, e.g. 'HIB 100' , 'SSLH 100' \n").upper()
+    times_open_list = []
 
-    ##    getClasses()  ##Use to update class database
+    #    getClasses()  # Use to update class database
     with open("WebSocFile.txt") as webSoc:
         for line in webSoc:
 
-            if (searchTerm in line and '@' not in line):
+            if search_term in line and '@' not in line:
                 line = line.strip()
                 line = line.split()
-                if 'M' in line[6] and 'W' not in line[6] and "F" not in line[6]:  ##Monday only classes
-                    timesOpenList.append('Monday' + str(line[7:9]))
-                if 'M' not in line[6] and 'W' in line[6] and "F" not in line[6]:  ##Wednesday only classes
-                    timesOpenList.append('Wednesday' + str(line[7:9]))
-                if 'M' not in line[6] and 'W' not in line[6] and "F" in line[6]:  ##Friday only classes
-                    timesOpenList.append('Friday' + str(line[7:9]))
-                if 'Tu' in line[6] and 'Th' not in line[6]:  ##Tuesday only classes
-                    timesOpenList.append('Tuesday' + str(line[7:9]))
-                if 'Tu' not in line[6] and 'Th' in line[6]:  ##Thursday only classes
-                    timesOpenList.append('Thursday' + str(line[7:9]))
+                if 'M' in line[6] and 'W' not in line[6] and "F" not in line[6]:  # Monday only classes
+                    times_open_list.append('Monday' + str(line[7:9]))
+                if 'M' not in line[6] and 'W' in line[6] and "F" not in line[6]:  # Wednesday only classes
+                    times_open_list.append('Wednesday' + str(line[7:9]))
+                if 'M' not in line[6] and 'W' not in line[6] and "F" in line[6]:  # Friday only classes
+                    times_open_list.append('Friday' + str(line[7:9]))
+                if 'Tu' in line[6] and 'Th' not in line[6]:  # Tuesday only classes
+                    times_open_list.append('Tuesday' + str(line[7:9]))
+                if 'Tu' not in line[6] and 'Th' in line[6]:  # Thursday only classes
+                    times_open_list.append('Thursday' + str(line[7:9]))
 
-                if 'M' in line[6] and 'W' in line[6] and "F" not in line[6]:  ##MW classes
-                    timesOpenList.append('Monday' + str(line[7:9]))
-                    timesOpenList.append('Wednesday' + str(line[7:9]))
-                if 'M' in line[6] and 'W' in line[6] and "F" in line[6]:  ##MWF classes
-                    timesOpenList.append('Monday' + str(line[7:9]))
-                    timesOpenList.append('Wednesday' + str(line[7:9]))
-                    timesOpenList.append('Friday' + str(line[7:9]))
-                if 'Tu' in line[6] and 'Th' in line[6]:  ##TuTh classes
-                    timesOpenList.append('Tuesday' + str(line[7:9]))
-                    timesOpenList.append('Thursday' + str(line[7:9]))
+                if 'M' in line[6] and 'W' in line[6] and "F" not in line[6]:  # MW classes
+                    times_open_list.append('Monday' + str(line[7:9]))
+                    times_open_list.append('Wednesday' + str(line[7:9]))
+                if 'M' in line[6] and 'W' in line[6] and "F" in line[6]:  # MWF classes
+                    times_open_list.append('Monday' + str(line[7:9]))
+                    times_open_list.append('Wednesday' + str(line[7:9]))
+                    times_open_list.append('Friday' + str(line[7:9]))
+                if 'Tu' in line[6] and 'Th' in line[6]:  # TuTh classes
+                    times_open_list.append('Tuesday' + str(line[7:9]))
+                    times_open_list.append('Thursday' + str(line[7:9]))
+
         print("These Are The Times The Room Has Class, \nTimes Not Listed Are The Times The Room Is Open")
-        timesOpenList = sorted(list(set(timesOpenList)))
-        output = open('TimesAvailablefor ' + searchTerm + '.txt', 'a')
+        times_open_list = sorted(list(set(times_open_list)))
 
-        with open('TimesAvailableFor ' + searchTerm + '.txt', 'a') as output:
-            for item in timesOpenList:
+        with open('TimesAvailableFor ' + search_term + '.txt', 'a') as output:
+            for item in times_open_list:
                 print(item)
                 output.write(item)
                 output.write("\n")
